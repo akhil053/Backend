334be25730a065001821d1d59ba6fd3625a6073b
AKhil <kr.akhil053@gmail.com>
2026-07-28 18:36:39 +0530

    Changes in utils

--- src/index.js
+++ src/index.js
@@ -3,7 +3,25 @@ import dotenv from "dotenv";
 import connectDB from "./db/index.js";
 dotenv.config({path:'./.env'});
 
-connectDB();
+
+
+
+connectDB()
+.then(() => {
+    const server = app.listen(process.env.PORT || 8000,()=>{
+        console.log(`Server is runnig on port ${process.env.PORT || 8000}`);
+    });
+
+    server.on("error",(error)=>{
+    console.log("Error during listening to the server",error);
+});
+
+})    
+
+
+.catch((error)=>{
+    console.log("MongoDB connecton failed",error);
+})
 
 
 
