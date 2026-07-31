d0dd1b06bb9c7e8f3e3c144878d7d0d3c0bcc440
AKhil <kr.akhil053@gmail.com>
2026-08-01 00:00:39 +0530

    Added register user pages

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 import express from "express";
 import cors from "cors";
 import cookieParser from "cookie-parser";
+import { ApiError } from "./utils/ApiError.js";
 
 const app = express();
 
@@ -16,6 +17,37 @@ app.use(express.urlencoded({extended : true , limit : "16kb"}))
 app.use(express.static("public"))
 app.use(cookieParser())
 
+
+//routes import
+
+import userRouter from "./routes/user.routes.js"  //import krte time naam kuch bhi rkh skte hai userRouter , router  , abc
+
+
+
+
+// routes declaration
+app.use("/api/v1/users",userRouter)
+
+
+// http://localhost:8000/api/v1/users/register
+
+app.use((err, req, res, next) => {
+    if (err instanceof SyntaxError && err.status === 400 && "body" in err) {
+        err = new ApiError(400, "Invalid JSON in request body")
+    }
+
+    const statusCode = err.statusCode || err.status || 500
+
+    return res.status(statusCode).json({
+        success: false,
+        message: err.message || "Internal server error",
+        errors: err.errors || []
+    })
+})
+
+
+
+export {app}
 
 
 
